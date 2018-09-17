98cf9f2b7b89f4fac0bc26f133c823dc68edaf39
yeon <bsuyeon16@gmail.com>
2018-09-17 20:15:44 +0900

    create 2 folder and coding

--- WarriorWars/Warrior.cs
+++ WarriorWars/Warrior.cs
@@ -1,6 +1,28 @@
-﻿namespace WarriorWars
+﻿using WarriorWars.Enum;
+using WarriorWars.Equipment;
+
+namespace WarriorWars
 {
     class Warrior
     {
+        private int goodGuyStrartingHealth;
+        private int badGuyStartingHealth;
+
+        private Faction faction;
+
+        private int health;
+        private string name;
+        private bool isAlive;
+
+        public bool IsAlive
+        {
+            get
+            {
+                return isAlive;
+            }
+        }
+
+        private Weapon weapon;
+        private Armor armor;
     }
 }
